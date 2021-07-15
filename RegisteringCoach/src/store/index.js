d3d3ebb339a6e069d31caacc150161278604583f
plamentsonev <87478146+plamentsonev@users.noreply.github.com>
2021-07-15 15:01:48 +0300

    Add files via upload

---
+++ RegisteringCoach/src/store/index.js
@@ -0,0 +1,14 @@
+import {createStore} from 'vuex';
+import coachesModules from './modules/coaches/index.js';
+import requestsModules from './modules/requests/index.js';
+import authModules from './modules/auth/index.js';
+
+const store = createStore({
+    modules: {
+        coaches: coachesModules,
+        requests: requestsModules,
+        auth: authModules
+    },   
+});
+
+export default store;
